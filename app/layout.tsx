f3d9bb49a64151416ab73597416efbb9d13cca87
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2026-01-14 12:15:46 +0600

    add suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased flex flex-col min-h-screen`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased flex flex-col min-h-screen`} suppressHydrationWarning>
         <ApolloWrapper>
           <AuthInitializer />
           <Navbar />
